aaeb718aeacfb96c96c20b3dd144ca07d39b5429
anand-rathore <av44314@gmail.com>
2021-05-05 17:21:59 +0530

    first commit

---
+++ src/main/java/com/example/Jwttutorial/MyController.java
@@ -0,0 +1,49 @@
+package com.example.Jwttutorial;
+
+import com.example.Jwttutorial.models.AuthenticationRequest;
+import com.example.Jwttutorial.models.AuthenticationResponse;
+import com.example.Jwttutorial.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MyController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+           throw new Exception("Incorrect username or password");
+        }
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+}
---
+++ src/test/java/com/example/Jwttutorial/JwtTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Jwttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
